e6371d811f409b83178962c3377d31e571586732
zidol <ajh3166@naver.com>
2022-04-14 21:42:17 +0900

    11. 깊이 우선 탐색(DFS)

--- src/search/depthFirstSearch/DfsSearch.java
+++ src/search/depthFirstSearch/DfsSearch.java
@@ -1,35 +1,27 @@
-package search.breadthFirstSearch;
+package search.depthFirstSearch;
+
+import search.breadthFirstSearch.BfsSearch;
 
 import java.util.*;
 
-public class BfsSearch {
+public class DfsSearch {
 
-    public List<String> bfsFunc(Map<String, List<String>> graph, String startNode) {
+    public List<String> dfsFunc(Map<String, List<String>> graph, String startNode) {
         List<String> visited = new ArrayList<>();
-        List<String> checkVisit = new ArrayList<>();
+        List<String> needVisit = new ArrayList<>();
+        needVisit.add(startNode);
 
-        checkVisit.add(startNode);
-        int count = 0;
-
-        //방문할 노드가 있는지 체크
-        while (checkVisit.size() > 0) {
-            count += 1;
-            String node = checkVisit.remove(0);
-
-            //방문을 하지 않았다면
+        while (needVisit.size() > 0) {
+            String node = needVisit.remove(needVisit.size() - 1);
             if (!visited.contains(node)) {
                 visited.add(node);
-                checkVisit.addAll(graph.get(node));
+                needVisit.addAll(graph.get(node));
             }
         }
-        //간선 수 + 노드 수 만큼 while 수행
-        System.out.println(count);
-        return visited;
+        return  visited;
     }
-
     public static void main(String[] args) {
         Map<String, List<String>> graph = new HashMap<String, List<String>>();
-
         graph.put("A", new ArrayList<String>(Arrays.asList("B", "C")));
         graph.put("B", new ArrayList<String>(Arrays.asList("A", "D")));
         graph.put("C", new ArrayList<String>(Arrays.asList("A", "G", "H", "I")));
@@ -40,7 +32,7 @@ public class BfsSearch {
         graph.put("H", new ArrayList<String>(Arrays.asList("C")));
         graph.put("I", new ArrayList<String>(Arrays.asList("C", "J")));
         graph.put("J", new ArrayList<String>(Arrays.asList("I")));
-        BfsSearch bfsSearch = new BfsSearch();
-        System.out.println(bfsSearch.bfsFunc(graph, "A"));
+        DfsSearch dfsSearch = new DfsSearch();
+        System.out.println(dfsSearch.dfsFunc(graph, "A"));
     }
 }
